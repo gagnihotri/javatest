1b6c60cdf0cc6cdbe2fdfe38a49ccdeb17ae60f0
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-14 16:59:58 +0530

    delete unnessary

--- todo-app/backend/src/test/java/com/reymar/todoapp/TodoAppApplicationTests.java
+++ todo-app/backend/src/test/java/com/reymar/todoapp/TodoAppApplicationTests.java
@@ -3,11 +3,11 @@ package com.reymar.todoapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TodoAppApplication.class)
 class TodoAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
+	
 }
