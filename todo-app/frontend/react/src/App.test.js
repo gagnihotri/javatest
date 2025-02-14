d0a685fbb33cc2dd0aeb50a316c128a7749d922e
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-14 20:16:02 +0530

    testing

--- todo-app/frontend/react/src/App.test.js
+++ todo-app/frontend/react/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Add task', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Add task/i);
   expect(linkElement).toBeInTheDocument();
 });
