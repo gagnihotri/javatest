60cd21954f4a44d327c09957a3c7b7a37b6548fd
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-14 17:57:11 +0530

    testing

--- todo-app/backend/src/test/java/com/reymar/todoapp/controller/TodoControllerTest.java
+++ todo-app/backend/src/test/java/com/reymar/todoapp/controller/TodoControllerTest.java
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@@SpringBootTest(TodoController.class)
+@@SpringBootTest(classes = TodoController.class)
 public class TodoControllerTest {
 
     @Autowired
